66632c420d925e3b42f3c7a8ee72e1468ee1c85f
Thiago-TP <thiagotomasdepaula@gmail.com>
2023-05-15 20:50:47 -0300

    pre processador agora espaça tokens e tira enter depois de label

--- pre_processador/PRE_PROCESSADOR.cpp
+++ pre_processador/PRE_PROCESSADOR.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
-int remove_blanks_and_comments     (const std::string&);
-int capitalize_text                (const std::string&);
-int equal_spacing_between_tokens   (const std::string&);
-int remove_enter_after_label       (const std::string&);
-int catch_double_label             (const std::string&);
-int catch_absent_text_section      (const std::string&);
-int catch_absent_begin_or_end      (const std::string&);
-int catch_lexic_error              (const std::string&);
+using namespace std;  // Evita usar "std::" toda hora
+
+int remove_blanks_and_comments     (const string&);
+int capitalize_text                (const string&);
+int equal_spacing_between_tokens   (const string&);
+int remove_enter_after_label       (const string&);
+int catch_double_label             (const string&);
+int catch_absent_text_section      (const string&);
+int catch_absent_begin_or_end      (const string&);
+int catch_lexic_error              (const string&);
 
 int main() 
 {
-    const std::string& source_file_name = "codigo_x85.txt";
+    const string& source_file_name = "codigo_x85.txt";  // Vai virar "argv"
     
-    if (remove_blanks_and_comments(source_file_name))   {return 1;}
-    if (capitalize_text("codigo_x85_limpo.txt"))        {return 1;}
-    // equal_spacing_between_tokens();
-    // remove_enter_after_label();
+    if (remove_blanks_and_comments(source_file_name))               {return 1;}
+    if (capitalize_text("codigo_x85_limpo.txt"))                    {return 1;}
+    if (equal_spacing_between_tokens("codigo_x85_caixa_alta.txt"))  {return 1;}
+    if (remove_enter_after_label("codigo_x85_espaçado.txt"))        {return 1;}
     // catch_double_label();
     // catch_absent_text_section();
     // catch_absent_begin_or_end();
@@ -28,77 +31,138 @@ int main()
 }
 
 
-int remove_blanks_and_comments(const std::string& filename)
+int sanity_check(ifstream& inputFile, ofstream& outputFile) 
 {
-    std::cout << "Limpando o código..." << std::endl;
-    std::ifstream inputFile(filename);
-    std::ofstream outputFile("codigo_x85_limpo.txt");
-
     if (!inputFile.is_open()) {
-        std::cout << "Falha ao abrir o arquivo de entrada." << std::endl;
+        cout << "Falha ao abrir o arquivo de entrada." << endl;
         return 1;
     }
-
     if (!outputFile.is_open()) {
-        std::cout << "Falha ao criar o arquivo de saída." << std::endl;
+        cout << "Falha ao criar o arquivo de saída." << endl;
         inputFile.close();
         return 1;
     }
-
-    std::string line;
-    while (std::getline(inputFile, line)) {
-
-        // Remove comentário (pega a substring até o ';')
-        size_t pos = line.find(';');
-        if (pos != std::string::npos) {
-            line = line.substr(0, pos);
-        }
-
-        // Não imprime linha em branco
-        if (!line.empty()) {
-            outputFile << line << std::endl;
-        }
-
-    }
-
-    inputFile.close();
-    outputFile.close();
-
-    std::cout << "Código limpado com sucesso.\n\n" << std::endl;
     return 0;
 }
 
 
-int capitalize_text(const std::string& filename) 
+int remove_blanks_and_comments(const string& filename)
 {
-    std::cout << "Deixando as linhas em maiúscula..." << std::endl;
-    std::ifstream inputFile(filename);
-    std::ofstream outputFile("codigo_x85_caixa_alta.txt");
+    cout << "Limpando o código..." << endl;
+    ifstream inputFile(filename);
+    ofstream outputFile("codigo_x85_limpo.txt");
 
-    if (!inputFile.is_open()) {
-        std::cout << "Falha ao abrir o arquivo fonte." << std::endl;
-        return 1;
-    }
+    if (sanity_check(inputFile, outputFile)) {return 1;} 
 
-    if (!outputFile.is_open()) {
-        std::cout << "Falha ao criar o arquivo de destino." << std::endl;
-        inputFile.close();
-        return 1;
-    }
+    string line;
+    while (getline(inputFile, line)) {
 
-    std::string line;
-    while (std::getline(inputFile, line)) {
-        for (char& c : line) {
-            if (std::isalpha(c)) {
-                c = std::toupper(c);
-            }
+        // Remove comentário (pega a substring até o ';')
+        size_t pos = line.find(';');
+        if (pos != string::npos) {
+            line = line.substr(0, pos);
         }
-        outputFile << line << std::endl;
+        // Não imprime linha em branco
+        if (!line.empty()) {
+            outputFile << line << endl;
+        }
     }
 
     inputFile.close();
     outputFile.close();
 
-    std::cout << "Linhas foram colocadas em maiúscula com sucesso.\n\n" << std::endl;
+    cout << "Código limpado com sucesso.\n\n" << endl;
+    return 0;
+}
+
+
+int capitalize_text(const string& filename) 
+{
+    cout << "Deixando as linhas em maiúscula..." << endl;
+    ifstream inputFile(filename);
+    ofstream outputFile("codigo_x85_caixa_alta.txt");
+
+    if (sanity_check(inputFile, outputFile)) {return 1;} 
+
+    string line;
+    while (getline(inputFile, line)) {
+        for (char& c : line) {
+            if (isalpha(c)) {
+                c = toupper(c);
+            }
+        }
+        outputFile << line << endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    cout << "Linhas foram colocadas em maiúscula com sucesso.\n\n" << endl;
+    return 0;
+}
+
+
+int equal_spacing_between_tokens(const string& filename)
+{
+    cout << "Espaçando o código..." << endl;
+    ifstream inputFile(filename);
+    ofstream outputFile("codigo_x85_espaçado.txt");
+
+    if (sanity_check(inputFile, outputFile)) {return 1;}    
+
+    string line;
+    while (getline(inputFile, line)) {
+
+        istringstream iss(line); 
+        string token;
+        while (iss >> token) {
+
+            // Separa os argumentos do copy com vírgula
+            size_t pos = token.find(',');
+            if (pos != string::npos) {
+                token = token.substr(0, pos) + " ,";
+            }
+
+            outputFile << token << " ";
+        }
+        outputFile << endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    cout << "Código espaçado com sucesso.\n\n" << endl;
+    return 0;
+}
+
+
+int remove_enter_after_label(const string& filename)
+{
+    cout << "Tirando enters depois de label..." << endl;
+    ifstream inputFile(filename);
+    ofstream outputFile("codigo_x85_labels_sem_enter.txt");
+
+    if (sanity_check(inputFile, outputFile)) {return 1;} 
+
+    string line;
+    while (getline(inputFile, line)) {
+
+        line.pop_back();  // O último caracter é sempre ' '
+        string label = line; 
+        
+        if (label.back() == ':') {
+            getline(inputFile, line);
+            outputFile << label << " " << line << endl;
+        }
+
+        else {
+            outputFile << line << endl;
+        }
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    cout << "Enters depois de label retirados com sucesso.\n\n" << endl;
     return 0;
 }
