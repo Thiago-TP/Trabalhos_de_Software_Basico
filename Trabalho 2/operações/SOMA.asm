06688834a268d48bf4eb713ae2ddbdb6fc182fd2
Cleyton Erick <118399769+CleytonErick@users.noreply.github.com>
2023-07-10 20:11:24 -0300

    Update SOMA.asm

--- Trabalho 2/operações/SOMA.asm
+++ Trabalho 2/operações/SOMA.asm
@@ -8,7 +8,65 @@ section .data
 extern  soma
 section .text
     soma:
+        push eax
         enter   0, 0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    ; Condições de soma
+
+        ; Se N2 menor que zero pula N2_neg
+        cmp [ebp+8],0
+        jl N2_neg
+
+        ; Se N1 menor que zero dado que N2 é positivo pula N2_pos_N1_neg
+        cmp [ebp+12],0
+        jmp N2_pos_N1_neg
+
+        ; Se ambos forem positivos
+        jmp Dois_pos
+
+        N2_neg:
+            ; Se N1 menor que zero dado que N2 é negativo pula Dois_neg
+            cmp [ebp+12],0
+            jl Dois_neg
+
+            ; Se N1 positivo dado que N2 é negativo pula N1_pos_N2_neg:
+            jmp N1_pos_N2_neg
         
-        leave
-        ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    ; Somas possíveis
+
+        ; Soma sem sinal
+        Dois_pos:
+            mov eax, [ebp+8]        ; eax = N2
+            add eax, [ebp+12]       ; eax = eax + N1 <- N2 +N1
+            jmp fim_soma
+
+        ; Somas com sinal
+        N1_pos_N2_neg:
+            neg [ebp+8]             ; N2 = -N2, torna N2 positivo pra subtrair posteriormente
+            mov eax, [ebp+12]       ; eax = N1
+            sub eax, [ebp+8]        ; eax = eax - N2 <- N1 - N2
+            jmp fim_soma
+
+        N2_pos_N1_neg:
+            neg [ebp+12]            ; N1 = -N1, torna N1 positivo pra subtrair posteriormente
+            mov eax, [ebp+8]        ; eax = N2
+            sub eax, [ebp+12]       ; eax = eax - N1 <- N2 - N1
+            jmp fim_soma
+
+        Dois_neg:
+            neg [ebp+8]             ; N2 = -N2, torna N2 positivo pra somar posteriormente
+            neg [ebp+12]            ; N1 = -N1, torna N1 positivo pra somar posteriormente
+            mov eax, [ebp+8]        ; eax = N2
+            add eax, [ebp+12]       ; eax = eax + N1 <- N2 +N1
+            neg eax                 ; eax = -eax, sinal da resposta
+            jmp fim_soma
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        fim_soma:
+            leave
+            pop eax
+            ret
