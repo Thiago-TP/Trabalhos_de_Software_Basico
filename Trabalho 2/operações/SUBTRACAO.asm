c895bcef22ecd1c384a848591e0569a7e67dbc23
Cleyton Erick <118399769+CleytonErick@users.noreply.github.com>
2023-07-16 16:16:33 -0300

    Update SUBTRACAO.asm

--- Trabalho 2/operações/SUBTRACAO.asm
+++ Trabalho 2/operações/SUBTRACAO.asm
@@ -1,74 +1,39 @@
-; seção de variáveis
-section .bss
+extern  precision
+extern  subtracao, getInt16, getInt32, putInt
 
-; seção de dados
-section .data 
+%include "io.mac"
 
-; seção de códigos
-extern  subtracao
-section .text
-    subtracao:
-        enter   0, 0
-        
-        push eax
-        enter   0, 0
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    
-    ; Condições de subtracao
+SECTION .text
+subtracao:
+    enter   0, 0
 
-        ; Se N2 menor que zero pula N2_neg_sub
-        cmp DWORD [ebp+8],0
-        jl N2_neg_sub
+    ;DECISÃO DE PRECISÃO
+    cmp DWORD [precision],1
+    jne subtracao16
 
-        ; Se N1 menor que zero dado que N2 é positivo pula N2_pos_N1_neg_sub
-        cmp DWORD [ebp+12],0
-        jmp N2_pos_N1_neg_sub
+    ;SUBTRAÇÃO32
+    call getInt32     
+    push eax                ; N1 empilhado        
+    call getInt32     
+    push eax                ; N2 empilhado
 
-        ; Se ambos forem positivos
-        jmp Dois_pos_sub
+    mov eax, DWORD [ebp-4]  ; eax = N1 
+    sub eax, DWORD [ebp-8]  ; eax = eax - N2 == N1 - N2
 
-        N2_neg_sub:
-            ; Se N1 menor que zero dado que N2 é negativo pula Dois_neg_sub
-            cmp DWORD [ebp+12],0
-            jl Dois_neg_sub
+    PutLInt eax
 
-            ; Se N1 positivo dado que N2 é negativo pula N1_pos_N2_neg_sub
-            jmp N1_pos_N2_neg_sub
-        
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    
-    ; Subtracoes possíveis
+    leave
+    ret 4
 
-        ; Subtracao sem sinal 
-        Dois_pos_sub:                ; N1 - N2
-            mov eax, [ebp+12]        ; eax = N1
-            sub eax, [ebp+8]         ; eax = eax - N2 <- N1 - N2
-            jmp fim_subtracao
 
-        ; Subtracao com sinal 
-        N1_pos_N2_neg_sub:          ; N1 - (-N2) = N1 + N2
-            neg DWORD [ebp+8]             ; N2 = -1*-N2, torna N2 positivo pra somar posteriormente
-            mov eax, [ebp+12]       ; eax = N1
-            add eax, [ebp+8]        ; eax = eax + N2 <- N1 + N2
-            jmp fim_subtracao
+    subtracao16:
+        call getInt16     
+        push eax                ; N1 empilhado        
+        call getInt16     
+        push eax                ; N2 empilhado
 
-        N2_pos_N1_neg_sub:          ; -N1 - N2 = -(N1 + N2)
-            neg DWORD [ebp+12]            ; N1 = -1*-N1, torna N1 positivo pra somar posteriormente
-            mov eax, [ebp+8]        ; eax = N2
-            add eax, [ebp+12]       ; eax = eax + N1 <- N2 + N1
-            neg eax                 ; -eax = -(N1 + N2)
-            jmp fim_subtracao
+        mov ax, WORD [ebp-4]  ; eax = N1 
+        sub ax, WORD [ebp-8]  ; eax = eax - N2 == N1 - N2        
 
-        Dois_neg_sub:               ; -N1 - (-N2) = N2 - N1 
-            neg DWORD [ebp+8]             ; N2 = -N2, torna N2 positivo pra subtrair posteriormente
-            neg DWORD [ebp+12]            ; N1 = -N1, torna N1 positivo pra subtrair posteriormente
-            mov eax, [ebp+8]        ; eax = N2
-            sub eax, [ebp+12]       ; eax = eax - N1 <- N2 - N1
-            jmp fim_subtracao
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-        fim_subtracao:
-            leave
-            pop eax
-            ret
+        leave
+        ret 4
