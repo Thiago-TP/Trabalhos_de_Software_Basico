dbbab8c736e947b2f80055606ff035970e786cdc
Cleyton Erick <118399769+CleytonErick@users.noreply.github.com>
2023-07-17 14:45:38 -0300

    Update EXPONENCIACAO.asm

--- Trabalho 2/operações/EXPONENCIACAO.asm
+++ Trabalho 2/operações/EXPONENCIACAO.asm
@@ -3,11 +3,114 @@ extern type_N1, type_N1_size, type_N2, type_N2_size, result_msg, result_msg_size
 extern  exponenciacao, getInt16, getInt32, putInt, putString
 extern  OF_warning, OF_warning_size, putString, end_program
 
-%include "io.mac"
 
 SECTION .text
-    exponenciacao:
-        enter   0, 0
-        
+exponenciacao:
+    enter   0, 0
+
+    push type_N1_size
+    push type_N1
+    call putString
+    pop eax 
+    pop eax
+
+    call getInt32     
+    push eax                ; N1 empilhado
+
+    push type_N2_size
+    push type_N2
+    call putString
+    pop eax 
+    pop eax
+
+    call getInt32     
+    push eax                ; N2 empilhado
+
+    sub eax, eax
+    sub ecx, ecx
+
+    mov eax, DWORD [ebp-4]  ; eax = N1 
+    mov ecx, DWORD [ebp-8]  ; ecx = N2
+
+    mov ebx, eax            ; eax = N1
+
+    ;Casos expeciais
+    cmp ecx, 0
+    je exp_zero
+    cmp ecx, 1
+    je fim_exp32
+
+    ;DECISÃO DE PRECISÃO
+    cmp DWORD [precision],1
+    jne exp16
+
+    ;EXPONENCIAÇÃO32
+    exp32:
+        imul ebx                ; edx.eax = eax*ebx == N1*N1
+        dec ecx
+        cmp ecx, 1
+        jg exp32
+
+        ;Analisa se edx foi somente extensão de sinal ou overflow
+        cmp edx,-1
+        je fim_exp32
+        cmp edx,0
+        je fim_exp32
+
+        jmp Exp_Overflow
+
+    exp_zero:
+        mov eax, 1
+    fim_exp32:
+
+        push eax
+
+        push result_msg_size
+        push result_msg
+        call putString
+        pop eax 
+        pop eax
+
+        call putInt
         leave
-        ret
+        ret 12
+    
+    exp16:
+        imul bx                ;dx.ax = ax*bx == N1*N1
+        dec cx
+        cmp cx, 1
+        jg exp16
+
+        ;Analisa se edx foi somente extensão de sinal ou overflow
+        cmp dx,-1
+        je fim_exp16
+        cmp dx,0
+        je fim_exp16
+
+        jmp Exp_Overflow
+
+    fim_exp16:
+
+        push eax
+
+        push result_msg_size
+        push result_msg
+        call putString
+        pop eax 
+        pop eax
+
+        call putInt
+        leave
+        ret 12
+
+    
+
+Exp_Overflow:
+    push OF_warning_size
+    push OF_warning
+
+    call putString
+
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
